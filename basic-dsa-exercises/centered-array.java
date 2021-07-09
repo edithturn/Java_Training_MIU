4dd1667b96525bc49c4fdecd7238a90601427416
Edith <edithpuclla20@gmail.com>
2021-07-08 21:55:32 -0500

    basics

---
+++ basic-dsa-exercises/centered-array.java
@@ -0,0 +1,28 @@
+/*Problem
+An array with an odd number of elements is said to be centered 
+if all elements (except the middle one) are strictly greater than
+the value of the middle element. Note that only arrays with an odd number of
+elements have a middle element. Write a function that accepts an integer array 
+and returns 1 if it is a centered array, otherwise it returns 0.
+
+Examples:
+if the input array is	return
+{1, 2, 3, 4, 5}	0 (the middle element 3 is not strictly less than all other elements)
+{3, 2, 1, 4, 5}	1 (the middle element 1 is strictly less than all other elements)
+{3, 2, 1, 4, 1}	0 (the middle element 1 is not strictly less than all other elements)
+{1, 2, 3, 4}	0 (no middle element)
+{}	0 (no middle element)
+{10}	1 (the middle element 10 is strictly less than all other elements)
+*/
+
+// Solution
+/*Check lengh , if length is odd number
+If length is zero return 0
+If length is one retutn 1
+Is length is not odd elements return 0
+It is odd number of elements, storage the middle.
+    Iterate all the numbers except the idex of the middle
+    If all the numbers are grather than middle return One (iterate until the end)
+    If a number is less than middle return Zero and skip
+*/
+
