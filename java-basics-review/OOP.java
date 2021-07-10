789fbe7e1de33903c0632544c366e7dc5fc94e5c
Edith <edithpuclla20@gmail.com>
2021-07-09 22:25:00 -0500

    OOP

--- java-basics-review/OOP.java
+++ java-basics-review/OOP.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
 
-class NestedStatement{
+class Main{
     public static void main(String[] args)
     {
+        base("ekaaaaaaaa", 4);
+        System.out.println(add2(6));
+    }
+public static void base(String str, int x){
+    for (int i = 0; i < x; i++){
+        System.out.println(str);
     }
 }
+public static int add2(int x){
+    return x + 2;
+        
+    }
+}
+
