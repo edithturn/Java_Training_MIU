34da8cb72bac57af2991c3492b219a8b1beab6b9
Edith <edithpuclla20@gmail.com>
2021-07-10 23:16:48 -0500

    class and objects

--- java-basics-review/classes/Dog.java
+++ java-basics-review/classes/Dog.java
@@ -7,9 +7,22 @@ public class Dog{
     public Dog(String name, int age){
         this.name = name;
         this.age = age;
+        add2();
     }
 
     public void speak(){
-        System.out.println("I am a dog, my name is +" + this.name + " " + this.age + "  Years old");
+        System.out.println("I am a dog, my name is " + this.name + " " + this.age + "  Years old");
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int age){
+        this.age  = age;
+    }
+
+    private int add2(){
+        return this.age;
     }
 }
--- java-basics-review/classes/Main.java
+++ java-basics-review/classes/Main.java
@@ -2,8 +2,21 @@
 public class Main{
 
     public static void main(String[] args){
-      Dog mydog = new Dog("firulais", 4);
-      mydog.speak();
+      Dog mydog1 = new Dog("Dog 01", 4);
+      mydog1.speak();
+
+      Dog mydog2 = new Dog("Dog 02", 7);
+      mydog2.speak();
+
+      Dog mydog3 = new Dog("Dog 03", 9);
+      mydog3.speak();
+
+      mydog1.speak();
+
+      mydog1.setAge(50);
+      mydog1.speak();
+      mydog1.add2();
+      
     }
 }
 
