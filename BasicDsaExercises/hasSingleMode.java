7d164fbc4c24c1a477272de4280a233e7de503ac
Edith <edithpuclla20@gmail.com>
2021-07-21 17:12:36 -0500

    hasSingleMode

--- BasicDsaExercises/hasSingleMode.java
+++ BasicDsaExercises/hasSingleMode.java
@@ -4,17 +4,17 @@ public  class hasSingleMode{
 
     public static void main(String [] args){
 
-        int[] arr = { 1, -29, 8, 5, -29, 6 };
-		//System.out.println(hasSingleMode(arr));
+        int[] arr = { 1, -29, 8, 5, -29, 6 }; // returns 1 
+		System.out.println(hasSingleMode(arr));
 
 		int[] arr2 = { 1, 2, 3, 4, 2, 4, 7 };
-		System.out.println(hasSingleMode(arr2));
+		System.out.println(hasSingleMode(arr2)); // returns 0
 
 		int[] arr3 = { 1, 2, 3, 4, 6 };
-		//System.out.println(hasSingleMode(arr3));
+		System.out.println(hasSingleMode(arr3)); // returns 0
 
 		int[] arr4 = { 7, 1, 2, 1, 7, 4, 2, 7 };
-		//System.out.println(hasSingleMode(arr4));
+		System.out.println(hasSingleMode(arr4)); // returns 1
     }
 
     static int hasSingleMode(int [] arr){
@@ -25,6 +25,7 @@ public  class hasSingleMode{
         int count = 0;
         int index = 0;
 
+        // To fill the array of the repetitions of a number in the original array
         for (int i=0; i< ans.length; i++){
             for (int j = 0; j < arr.length; j++){
                 if (ans[i] == arr[j]){
@@ -35,29 +36,36 @@ public  class hasSingleMode{
             index += 1;
             count = 0;
         }
-
-        System.out.println(Arrays.toString(mode)); //[1, 2, 1, 2, 1]  //[1, 2, 1, 1, 1]
         
-        int max = ans[0];
-        for (int i = 1; i < ans.length; i++){
-            if (ans[i] > max){
-                max = ans[i]; 
+        // Examples mod = [1, 2, 1, 2, 1] , mod = [1, 2, 1, 1, 1]
+        // After get the mod array which containes the number of times a number is repeated
+        // Calculate the max value of that array
+        System.out.println(" ");
+        System.out.println("Unique elements " + Arrays.toString(ans));
+        System.out.println("Mode of each element " + Arrays.toString(mode));
+        
+        int max = mode[0];
+        for (int i = 1; i < mode.length; i++){
+            if (mode[i] > max){
+                max = mode[i]; 
             }
         }
-
+        // Increase in one if the max value repets one time on the array, it means it appears just one 
         int duplicates = 0;
-        for (int i = 0; i < ans.length; i++){
-            if(ans[i] == max){
+        for (int i = 0; i < mode.length; i++){
+            if(mode[i] == max){
                 duplicates += 1;
             }
         }
-        if(duplicates > 1){
-            return 0;
-        }else{
+        // If duplicate occurs just One then return 1, if occurs more than One, return 0
+        if(duplicates == 1){
             return 1;
+        }else{
+            return 0;
         }       
     }
 
+    // It will return  the unique elements in the original array
     static int [] findUniqueElements(int[] a){
         int index = 0;
         int [] ans = new int[uniqueElements(a)];
@@ -75,9 +83,10 @@ public  class hasSingleMode{
                 index += 1;
             }
         }
-        System.out.println(Arrays.toString(ans));
         return ans;
     }
+
+    // it will return the number of unique elements
     static int uniqueElements(int[] a){
         int count = 0;
         
@@ -93,7 +102,6 @@ public  class hasSingleMode{
                 count += 1;
             }
         }
-        System.out.println(count);
         return count;
 }
 }
