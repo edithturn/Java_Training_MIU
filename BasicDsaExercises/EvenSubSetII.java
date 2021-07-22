1124c088fe12482f0b60dfddf57968511cb0df4a
Edith <edithpuclla20@gmail.com>
2021-07-22 18:02:23 -0500

    even subset II

---
+++ BasicDsaExercises/EvenSubSetII.java
@@ -0,0 +1,29 @@
+public class EvenSubsetII{
+
+    public static void main(String [] args){
+
+        System.out.println(isEvenSubset(18, 12)); // 1
+        System.out.println(isEvenSubset(18, 32)); // 0
+        System.out.println(isEvenSubset(16, 24)); // 1
+
+    }
+    static int isEvenSubset(int m , int n){
+        /*
+        m = [ 6 2 ]
+        n = [ 6 4 2 ]
+        */
+
+        int result = 0;
+
+        for (int i = 2; i < m/2; i+=2){
+            if (m % i == 0){
+                if (n % i == 0){
+                    result = 1;
+                } else{
+                    result = 0;
+                }
+            }             
+        }
+        return result;
+    }
+}
