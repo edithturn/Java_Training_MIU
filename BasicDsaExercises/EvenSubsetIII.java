f8469a221127bfa8fb753bec6d954b81b4125adb
Edith <edithpuclla20@gmail.com>
2021-07-23 23:01:21 -0500

    EvenSubIII

--- BasicDsaExercises/EvenSubsetIII.java
+++ BasicDsaExercises/EvenSubsetIII.java
@@ -1,8 +1,4 @@
-// An integer is defined to be an even subset of another integer n if every even factor of m is also a  factor of n. 
-// For example 18 is an even subset of 12 because the even factors of 18 are 2 and 6 and  these are both factors of 12. 
-// But 18 is not an even subset of 32 because 6 is not a factor of 32. 
-// Write a function with signature int isEvenSubset(int m, int n) that returns 1 if m is an even subset  of n,
-// otherwise it returns 0.
+//EvenSubsetIII - Optimized
 
 // Example 01
 // m = 18: 18 9 6 3 2 1 | 6 2 => return 1
