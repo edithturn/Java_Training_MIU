8356155af96cf8bdcdd5f26b9742d92a65cc2b5e
Edith <edithpuclla20@gmail.com>
2021-08-17 12:40:42 -0500

    CountDigitI

---
+++ MostFrecuentlyAsked/CountDigitI.java
@@ -0,0 +1,24 @@
+class CountDigit{
+    public static void main(String [] args){
+
+        System.out.println(countDigit(32121,1));  // 2
+        System.out.println(countDigit(33331, 3)); // 4
+        System.out.println(countDigit(33331, 6)); // 0
+        System.out.println(countDigit(3, 3));     // 1
+
+    }
+
+    static int countDigit(int n, int digit){
+
+        int count = 0;
+
+        while(n > 0){
+            if(n%10 == digit)
+                count++;
+            n = n/10;
+        }
+        return count;
+
+    }
+
+}
