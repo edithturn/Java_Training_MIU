3054ce9a1870ac096eaba767b3ce743ff4895a17
Edith <edithpuclla20@gmail.com>
2021-09-02 21:21:44 -0500

    EvenSpaced

--- MostFrecuentlyAsked/EvenSpaced.java
+++ MostFrecuentlyAsked/EvenSpaced.java
@@ -19,28 +19,16 @@ class EvenSpaced{
     }
     static int isEvenSpaced(int[] a){
 
-     int tmp = 0;
+        int largest = a[0];
+        int smallest = a[1];
 
-     for(int i = 0; i < a.length; i++){
-         for(int j = 0; j < a.length; j++){
-
-             if(a[j] < a[i]){
-                 tmp = a[i];
-                 a[i]  = a[j];
-                 a[j] = tmp;
-                }
-            }
+        for(int i = 2; i < a.length; i++){
+            if(a[i] > largest)
+                largest = a[i];
+            if(a[i] < smallest)
+                smallest = a[i];
         }
-    int largest = a[0];
-    int smallest = a[a.length - 1];
 
-    //System.out.println(Arrays.toString(a));
-    //System.out.println((largest));
-    //System.out.println((smallest));
-
-    if((largest - smallest) % 2 == 0)
-        return 1;
-    else
-        return 0;
+    return (largest - smallest) % 2 == 0 ? 1 : 0;
     }
 }
