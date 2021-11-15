b5db94641d759a468631ec8511d0f598eb59bd0f
edithturn <edypuclla@gmail.com>
2021-11-14 21:54:34 -0500

    logical operators

---
+++ java-tutorial-for-beginners/ExampleOperatorsRelationals.java
@@ -0,0 +1,17 @@
+public class ExampleOperatorsRelationals {
+
+    public static void main(String[] args){
+
+        int a = 6;
+        int b = 0;
+        
+        if(a > 3 && !(b < 1))
+        {
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+    
+}
