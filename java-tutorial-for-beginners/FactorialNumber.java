d529c322d102fc0abc70ec636596e4d4cd005725
edithturn <edypuclla@gmail.com>
2021-11-19 22:43:48 -0500

    Factorial Number

---
+++ java-tutorial-for-beginners/FactorialNumber.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.print.event.PrintEvent;
+
+public class FactorialNumber {
+
+    public static void main(String[] args) throws NumberFormatException, IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Type a number : ");
+        int number = Integer.parseInt(br.readLine());
+        int total = 1;
+
+        while(number < 0){
+            System.out.println("ERROR: Number is not positive.");
+
+            System.out.print("Write a number: ");
+            number = Integer.parseInt(br.readLine());
+        }
+        while(number >= 1){
+            total = total*number;
+            number -= 1;
+        }
+        System.out.println("Factorial of giving number is " + total);    
+    }
+    
+}
+
