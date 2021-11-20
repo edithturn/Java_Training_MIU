e5743ca30d546bef56af718521487a33cbe89b18
edithturn <edypuclla@gmail.com>
2021-11-19 23:02:19 -0500

    Exmaple functions

---
+++ java-tutorial-for-beginners/ExampleFunctions.java
@@ -0,0 +1,13 @@
+public class ExampleFunctions {
+    static int multiplicationByThree(int p){
+        int r = p *3;
+        return r;
+
+    }
+    public static void main(String[] aStrings){
+         int a = 5;
+         int b = multiplicationByThree(a);
+         System.out.println(b);
+    }
+    
+}
